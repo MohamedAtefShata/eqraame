1978d97d5910f0e73f1df4aa72a085e2907ed86f
hngara <midomada45@gmail,com>
2022-12-08 15:50:01 +0200

    start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Nav from "./components/nav.component"
+import home from "./components/home.component";
+import CreateUser from "./components/createuser.component.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+      <Nav />
+      <br/>
+      <Route path="/" exact component={home} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
   );
 }
 
