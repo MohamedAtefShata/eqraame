8041a93354dc280773f7d6b514688b222fd4d29e
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-15 21:50:27 +0200

    add wallet model

---
+++ backend/src/models/Wallet.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const walletScheme = new mongoose.Schema(
+  {
+    user_id: { type: mongoose.Schema.ObjectId, ref: "User", required: true },
+    balance: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    courses: { type: [mongoose.Schema.Types.ObjectId] },
+  },
+  { timestamps: true }
+);
+
+walletScheme.methods.addCourse = function (course) {
+  this.courses.unshift(course);
+};
+
+module.exports = mongoose.model("Wallet", walletScheme);
