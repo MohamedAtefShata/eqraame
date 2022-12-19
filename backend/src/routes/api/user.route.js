edd9b1e60362b9bc8a8776850ed1d3069ecdfbf6
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-19 04:07:39 +0200

    create cheakers middleware and seprate cheakers

--- backend/src/routes/api/user.route.js
+++ backend/src/routes/api/user.route.js
@@ -5,7 +5,10 @@
  */
 
 const router = require("express").Router();
-const { check, validationResult } = require("express-validator");
+const {
+  checkUserRegistration,
+  validateCheckers,
+} = require("../../middlewares/cheakers");
 const User = require("../../models/User.model");
 const userService = require("../../services/user.service");
 
@@ -19,45 +22,11 @@ router.post(
   "/register",
 
   /*****  checkers *****/
-  check("name")
-    .trim()
-    .not()
-    .isEmpty()
-    .withMessage("Name is require")
-    .escape()
-    .matches(/^[a-zA-Z\s\.\-]+$/g)
-    .withMessage("Invalid name (use only letters , white space and (. or -))")
-    .isLength({ max: 100 })
-    .withMessage("Name must be at most 100 character"),
-
-  check("email").trim().isEmail().withMessage("Invaild E-mail"),
-
-  check("password")
-    .not()
-    .isEmpty()
-    .withMessage("Password is required")
-    .isLength({ min: 8, max: 50 })
-    .withMessage(
-      "password must be at least 8 characters and at most 50 character"
-    )
-    .matches(/\d/)
-    .withMessage("password must contain number"),
-  check("confirm-password", "confirm password is not match password").custom(
-    (value, { req }) => value === req.body.password
-  ),
-
-  check("role")
-    .trim()
-    .custom(
-      (value) => !value || ["student", "teacher", "admin"].includes(value)
-    )
-    .withMessage("invalid role propartey"),
+  checkUserRegistration,
+  // validate chekers is passed
+  validateCheckers,
   /******** Response handling ********/
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-      return res.status(401).json({ errors: errors.array() });
-
     const { name, email, password, birthdate, role, avatar } = req.body;
 
     try {
