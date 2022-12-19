91a5164074413cfaa5979b28a3680c85aace1125
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-19 03:26:20 +0200

    solve course model problems and add lessons to course

--- backend/src/models/Course.model.js
+++ backend/src/models/Course.model.js
@@ -1,19 +1,34 @@
 const mongoose = require("mongoose");
 
 const CourseScheme = new mongoose.Schema(
-    {
-        name: { type: String, required: true, trim: true },
-        author_id: {
-            type: mongoose.Schema.ObjectId,
-            ref: "Author",
-            required: true,
-            immutable: true,
-            index: true,
-          },
-        descreption:{
-            type: String,
-        }
+  {
+    name: { type: String, required: true, trim: true },
+    author_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+      immutable: true,
+      index: true,
     },
+    descreption: {
+      type: String,
+    },
+    lessons: [
+      {
+        index: { type: Number, required: true },
+        name: { type: String, required: true, trim: true },
+        content_type: {
+          type: String,
+          enum: ["article", "video"],
+          default: "article",
+        },
+        content: {
+          type: String,
+        },
+      },
+    ],
+  },
   { timestamps: true }
 );
+
 module.exports = mongoose.model("Course", CourseScheme);
--- backend/src/models/Lesson.model.js
+++ backend/src/models/Lesson.model.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
 
 const CourseScheme = new mongoose.Schema(
-    {
-        name: { type: String, required: true, trim: true },
-        course_id: {
-            type: mongoose.Schema.ObjectId,
-            ref: "Author",
-            required: true,
-            immutable: true,
-            index: true,
-          },
-        content_type:{
-            type: String
-        },
-        content:{
-            type: String
-        }
-    },{ timestamps: true }
+  {
+    name: { type: String, required: true, trim: true },
+    course_id: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Course",
+      required: true,
+      immutable: true,
+      index: true,
+    },
+    content_type: {
+      type: String,
+      enum: ["article", "video"],
+      default: "article",
+    },
+    content: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 module.exports = mongoose.model("Course", CourseScheme);
