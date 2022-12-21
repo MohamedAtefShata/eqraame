4cfde2e536eed38bd0de7f5cf1596f386c429a5b
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-21 02:02:12 +0200

    change delete method

--- backend/src/routes/api/course.route.js
+++ backend/src/routes/api/course.route.js
@@ -74,6 +74,48 @@ router.get(
     }
   }
 );
+/**
+ * @route DELETE /api/course/delete/:id
+ * @acess private
+ * @desc  delete course by id
+ */
+router.delete(
+  // path
+  "/:id",
+  auth,
+  teacherAuth,
+
+  /******** Response handling ********/
+  async (req, res) => {
+    try {
+      // validate request
+      const course_id = req.params.id;
+      let course = await CourseModel.findById(course_id);
+      if (!course) throw new BadRequestError("Invalid Course ID");
+
+      if (!course.author_id.equals(req.user.id))
+        throw new BadRequestError("You don't have access to delete", 401);
+
+      // delete
+      await CourseModel.findByIdAndDelete(course_id);
+
+      return res.json({ msg: "deleted successfuly" });
+    } catch (error) {
+      // if id is not object id
+      if (error.kind === "ObjectId")
+        error = BadRequestError("Invalid Course ID");
+
+      if (error.name === "BadRequest")
+        return res.status(error.status).json(error.json);
+
+      console.log(
+        "error in get courses :",
+        `< ${error.name} >:${error.message}`
+      );
+      return res.status(500).json({ msg: "server error" });
+    }
+  }
+);
 
 /**
  * @route POST /api/course
