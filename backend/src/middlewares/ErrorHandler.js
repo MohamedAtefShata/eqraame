76c9d2ff3f6d8a66c670353d8e4b3adf6c72e4ac
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-22 19:25:34 +0200

    add error handler and response error

---
+++ backend/src/middlewares/ErrorHandler.js
@@ -0,0 +1,19 @@
+/**
+ * Error Handler middleware
+ * @description Erro handler middleware to check if there error in final request to retutn response
+ * @author Mahmoud Atef
+ */
+
+module.exports = (error, req, res, next) => {
+  const status = error.status || 500;
+  const location = error.location || "";
+  const name = error.name || "Error";
+  const message = error.message || "";
+  const errors = error.json || [{ msg: "Internal server error" }];
+  const stack = error.stack;
+
+  // log and response
+  console.log(`error happend in (${location}) : < ${name}> : ${message}`);
+  console.log(`stack : ${stack}`);
+  return res.status(status).json({ errors });
+};
