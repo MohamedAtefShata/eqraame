505d457d5ca1966b26bd6d5c40ba4575d378e522
Mahmoud Atef <mahmoudatef.coder@gmail.com>
2022-12-24 22:37:52 +0200

    teach k3br javascript

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 //add
-const signup = (name,email, password,birthdate,role) => {
+const signup = (name, email, password, birthdate, role) => {
   return axios
     .post("http://localhost:5000/api/user/register", {
-      ["name"]:name,
-      ["email"]:email,
-      ["password"]:password,
-      ["confirm-password"] : password,
-      ["birthdate"]:birthdate,
-      ["role"]:role.toLowerCase(),
-      ["avatar"]:"default"
+      name,
+      email,
+      password,
+      ["confirm-password"]: password,
+      birthdate,
+      role: role.toLowerCase(),
+      avatar: "default",
     })
     .then((response) => {
-        if (response.data.token) {
-            localStorage.setItem("user", response.data.token);
-        }
-        return response.data.token;
+      if (response.data.token) {
+        localStorage.setItem("user", response.data.token);
+      }
+      return response.data.token;
     });
 };
 
