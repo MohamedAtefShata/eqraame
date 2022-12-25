6b335a8bead646b8aba7266fb6778dee388af760
hngara <midomada45@gmail.com>
2022-12-25 03:18:54 +0200

    loading component

--- frontend/src/components/Pages/EditProfile.js
+++ frontend/src/components/Pages/EditProfile.js
@@ -6,6 +6,7 @@ import "../Styles/App.css";
 import "../Styles/EditProfile.css";
 import ProfileTemp from "../ProfileTemp";
 import { Button1 } from "../Button1";
+import '../Styles/loading.css'
 
 function EditProfile() {
   const [userinfo, setuserinfo] = useState([]);
@@ -13,17 +14,20 @@ function EditProfile() {
   const [password, setPassword] = useState("");
   const [birthdate, setBirthdate] = useState("");
   const [name, setName] = useState("");
+  const [loading, setloading] = useState(true);
   const [selected, setSelected] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
+    setloading(true);
     PostService.getuserinfo().then(
       (response) => {
-        console.log(response);
+        setloading(false);
         setuserinfo(response.data.user);
       },
       (error) => {
         console.log("Private page", error.response);
+        setloading(false);
         // Invalid token
         if (error.response && error.response.status !== 200) {
           AuthService.logout();
@@ -33,6 +37,21 @@ function EditProfile() {
       }
     );
   }, []);
+  if(loading){
+    return( 
+      <>
+      <div id="wrapper">
+          <div className="profile-main-loader">
+              <div className="loader">
+                  <svg className="circular-loader"viewBox="25 25 50 50" >
+                      <circle className="loader-path" cx="50" cy="50" r="20" fill="none" stroke="#70c542" stroke-width="2" />
+                  </svg>
+              </div>
+          </div>
+      </div>
+      </>
+      )
+  }
   return (
     <>
       <div className="editProfile-container">
