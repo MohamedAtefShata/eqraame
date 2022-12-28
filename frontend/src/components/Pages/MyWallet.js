adb9ec38f4a7a8376df45de4f627b68e6bb26e28
Abdel Rahman Aboelyazeed <76536292+aboelyazeed@users.noreply.github.com>
2022-12-29 01:51:39 +0200

    Add MyWallet page

---
+++ frontend/src/components/Pages/MyWallet.js
@@ -0,0 +1,13 @@
+import "../Styles/App.css";
+import React from "react";
+import Wallet from "../Wallet";
+
+function MyWallet() {
+  return (
+    <>
+      <Wallet />
+    </>
+  );
+}
+
+export default MyWallet;
