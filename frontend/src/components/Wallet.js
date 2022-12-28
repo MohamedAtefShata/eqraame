e99a8e31f7a78241aa199b3ea3f324a13da57b56
Abdel Rahman Aboelyazeed <76536292+aboelyazeed@users.noreply.github.com>
2022-12-29 01:52:05 +0200

    Add Wallet componant

---
+++ frontend/src/components/Wallet.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import PostService from "../services/post.service";
+import AuthService from "../services/auth.service";
+import { useNavigate } from "react-router-dom";
+import { Button } from "./Button";
+import "./Styles/loading.css";
+import "./Styles/Wallet.css";
+import ProfileTemp from "./ProfileTemp";
+
+function Wallet() {
+  const [userinfo, setuserinfo] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [name, setName] = useState("");
+  const [loading, setloading] = useState(true);
+  const [selected, setSelected] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setloading(true);
+    PostService.getuserinfo().then(
+      (response) => {
+        setloading(false);
+        setuserinfo(response.data.user);
+      },
+      (error) => {
+        console.log("Private page", error.response);
+        setloading(false);
+        // Invalid token
+        if (error.response && error.response.status !== 200) {
+          AuthService.logout();
+          navigate("/login");
+          window.location.reload();
+        }
+      }
+    );
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <div id="wrapper">
+          <div className="profile-main-loader">
+            <div className="loader">
+              <svg className="circular-loader" viewBox="25 25 50 50">
+                <circle
+                  className="loader-path"
+                  cx="50"
+                  cy="50"
+                  r="20"
+                  fill="none"
+                  stroke="#70c542"
+                  stroke-width="2"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="mywallet-container">
+        <ProfileTemp />
+        <div className="center-handel">
+          <div className="wallet-Header">
+            <h1 className="header">My Wallet</h1>
+            <h3 className="wallet-disc">
+              Wallet balance and charge more money
+            </h3>
+          </div>
+          <div className="wallet-contanier">
+            <p className="wallet-balance">
+              <b className="balance">{"Your Balance: "}</b> 154$
+            </p>
+            <form>
+              <Button
+                className="btns"
+                buttonStyle="btn--primary--logsign"
+                buttonSize="btn--medium"
+                buttonTrans="btn--logsign"
+                buttonPath="/charging"
+              >
+                Charge Your Wallet
+              </Button>
+            </form>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+}
+
+export default Wallet;
