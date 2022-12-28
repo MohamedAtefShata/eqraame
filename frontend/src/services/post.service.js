0e2e550680f790d9bd1fc6f6751758f1905d874b
hngara <midomada45@gmail.com>
2022-12-28 17:02:34 +0200

    adding getcourse postservice

--- frontend/src/services/post.service.js
+++ frontend/src/services/post.service.js
@@ -2,14 +2,17 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 const getuserinfo = () => {
-  return axios.get("http://localhost:5000/api/auth",{ headers: authHeader()});
+  return axios.get("http://localhost:5000/api/auth", { headers: authHeader() });
 };
 const getcourseinfo = () => {
   return axios.get("http://localhost:5000/api/course");
 };
-
+const getcourse = () => {
+  return axios.get("http://localhost:5000/api/course/63a235ccb38c8720b8130199");
+};
 const postService = {
-    getuserinfo,
-    getcourseinfo
+  getuserinfo,
+  getcourseinfo,
+  getcourse,
 };
 export default postService;
